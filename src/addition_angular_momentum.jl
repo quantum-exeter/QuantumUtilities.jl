ab253337078197186acbe3d14086a14659108dbb
Federico Cerisola <federico@cerisola.net>
2024-07-30 21:51:38 +0100

    Add docstring to angular mometum addition function

--- src/addition_angular_momentum.jl
+++ src/addition_angular_momentum.jl
@@ -1,3 +1,15 @@
+"""
+    add(S1::SpinLength, S2::SpinLength)
+
+Return a tuple of the different possible total angular momentum values resulting from the addition of two angular momenta `S1` and `S2`.
+
+# Arguments
+- `S1::SpinLength`: The first angular momentum.
+- `S2::SpinLength`: The second angular momentum.
+
+# Returns
+- A tuple containing all possible values of total angular momentum.
+"""
 function add(S1::SpinLength{N,D}, S2::SpinLength{M,P}) where {N,D,M,P}
     isless(S1, S2) && return +(S2, S1)
     return _sum(S1, S2)
